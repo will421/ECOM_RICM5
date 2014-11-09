5ef3e7579f3cbf2e7acf2ec750ab5d11ee85e596
jejeLaVedette <PolytechRICM@192.168.1.81>
2014-11-09 22:36:07 +0100

    creation du projet Ecom avec les sous dossier

---
+++ Ecom/ecomapp/src/main/java/ecom/ejb/client/Main.java
@@ -0,0 +1,12 @@
+package ecom.ejb.client;
+
+/**
+ * Enterprise Application Client main class.
+ *
+ */
+public class Main {
+    
+    public static void main( String[] args ) {
+        System.out.println( "Hello World Enterprise Application Client!" );
+    }
+}
---
+++ Ecom/src/main/java/Ecom/Ecom/App.java
@@ -0,0 +1,13 @@
+package Ecom.Ecom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
