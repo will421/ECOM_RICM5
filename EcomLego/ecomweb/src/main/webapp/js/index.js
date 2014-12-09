94b4b1d4283de54bbfea7b7d89922564e108c3bf
umpri5450 <umpri5450@gmail.com>
2014-12-09 08:59:45 +0100

    Pages dynamiques et panier d'achat

--- EcomLego/ecomweb/src/main/webapp/js/index.js
+++ EcomLego/ecomweb/src/main/webapp/js/index.js
@@ -1,125 +1,79 @@
-var app = angular.module('appMain', ['ui.bootstrap','ngResource'], function($httpProvider) {
-  // Use x-www-form-urlencoded Content-Type
-  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+var app = angular.module('appMain', ['ui.bootstrap', 'ngResource', 'ui.router', 'ngCart'], function ($httpProvider) {
+    // Use x-www-form-urlencoded Content-Type
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 
-  /**
-   * The workhorse; converts an object to x-www-form-urlencoded serialization.
-   * @param {Object} obj
-   * @return {String}
-   */ 
-  var param = function(obj) {
-    var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+    /**
+     * The workhorse; converts an object to x-www-form-urlencoded serialization.
+     * @param {Object} obj
+     * @return {String}
+     */
+    var param = function (obj) {
+        var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
 
-    for(name in obj) {
-      value = obj[name];
+        for (name in obj) {
+            value = obj[name];
 
-      if(value instanceof Array) {
-        for(i=0; i<value.length; ++i) {
-          subValue = value[i];
-          fullSubName = name + '[' + i + ']';
-          innerObj = {};
-          innerObj[fullSubName] = subValue;
-          query += param(innerObj) + '&';
+            if (value instanceof Array) {
+                for (i = 0; i < value.length; ++i) {
+                    subValue = value[i];
+                    fullSubName = name + '[' + i + ']';
+                    innerObj = {};
+                    innerObj[fullSubName] = subValue;
+                    query += param(innerObj) + '&';
+                }
+            }
+            else if (value instanceof Object) {
+                for (subName in value) {
+                    subValue = value[subName];
+                    fullSubName = name + '[' + subName + ']';
+                    innerObj = {};
+                    innerObj[fullSubName] = subValue;
+                    query += param(innerObj) + '&';
+                }
+            }
+            else if (value !== undefined && value !== null)
+                query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
         }
-      }
-      else if(value instanceof Object) {
-        for(subName in value) {
-          subValue = value[subName];
-          fullSubName = name + '[' + subName + ']';
-          innerObj = {};
-          innerObj[fullSubName] = subValue;
-          query += param(innerObj) + '&';
-        }
-      }
-      else if(value !== undefined && value !== null)
-        query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
-    }
 
-    return query.length ? query.substr(0, query.length - 1) : query;
-  };
-
-  // Override $http service's default transformRequest
-  $httpProvider.defaults.transformRequest = [function(data) {
-    return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
-  }];
-});
-/*
-Main dropdown menu controller
-*/
-/*
-app.controller('DropdownCtrl', function ($scope, $log, LaPoste) {
-
-    console.log("HERE");
-    LaPoste.query(
-        function(data){
-                       console.log("HERE");
-                       console.log(data);
-                      }
-    );
-    
-    $scope.items = [
-        'The first choice!',
-        'And another choice for you.',
-        'but wait! A third!'
-    ];
-    
-    
-    $scope.status = {
-        isopen: false
+        return query.length ? query.substr(0, query.length - 1) : query;
     };
 
-    $scope.toggled = function (open) {
-        $log.log('Dropdown is now: ', open);
-    };
+    // Override $http service's default transformRequest
+    $httpProvider.defaults.transformRequest = [function (data) {
+            return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+        }];
 });
-*/
+/**
+ * Controller menu navigation principal (Dropdown)
+ */
 app.controller('DropdownCtrl', function ($scope, $log, $http) {
-
-    console.log("HERE");
     $scope.items = [
         'The first choice!',
         'And another choice for you.',
         'but wait! A third!'
     ];
-    
-    $http.get('http://127.0.0.1:5000/todos/todo1').
-        success(function(data, status, headers, config) {
-            $scope.items[0] = data;
-        }).error(function(data, status, headers, config){
-            console.log("GET FAILED");
-    });
     /*
-    $http.post('http://127.0.0.1:5000/todos', {'msg1':'SWAGGER'}).
-        success(function(data, status, headers, config) {
-           console.log('POST OK');
-        }).error(function(data, status, headers, config){
-            console.log("POST FAILED");
-            console.log(headers);
-    });
-    */
-    
-    /*         headers: {
-           'Content-Type': undefined
-         },
-         http://victorblog.com/2012/12/20/make-angularjs-http-service-behave-like-jquery-ajax/
-         
-         */
-    var req = {
-         method: 'POST',
-         url: 'http://127.0.0.1:5000/todos',
-
-        
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-         data: { msg : "SWAGGER"}.toJSON
-    }
-    
-    $http(req).success(function(data, status, headers, config) {
-        console.log('POST OK !!');
-        console.log(data);
-        }).error(function(data, status, headers, config){
-    });
-    
-    
+     $http.get('http://127.0.0.1:5000/todos/todo1').
+     success(function (data, status, headers, config) {
+     $scope.items[0] = data;
+     }).error(function (data, status, headers, config) {
+     console.log("GET FAILED");
+     });
+     
+     var req = {
+     method: 'POST',
+     url: 'http://127.0.0.1:5000/todos',
+     headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+     data: {msg: "SWAGGER"}.toJSON
+     }
+     
+     $http(req).success(function (data, status, headers, config) {
+     console.log('POST OK !!');
+     console.log(data);
+     }).error(function (data, status, headers, config) {
+     });
+     
+     */
     $scope.status = {
         isopen: false
     };
@@ -130,9 +84,9 @@ app.controller('DropdownCtrl', function ($scope, $log, $http) {
 });
 
 
-/*
-Carousel controller
-*/
+/**
+ * Controller carousel
+ */
 app.controller('CarouselDemoCtrl', function ($scope) {
     $scope.myInterval = 5000;
     $scope.slides = [
@@ -151,7 +105,66 @@ app.controller('CarouselDemoCtrl', function ($scope) {
     ];
 });
 
+/**
+ * Configuration Angular UI Router
+ */
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+            // HOME STATES AND NESTED VIEWS ========================================
+            .state('#', {
+                url: '/home',
+                templateUrl: 'partial-home.html'
+            })
+
+            .state('produits', {
+                url: 'produits',
+                templateUrl: 'partial-produits.html'
+            })
+
+            .state('imprimer-commanderundevis', {
+                url: 'imprimer-commanderundevis',
+                templateUrl: 'partial-imprimer-commanderundevis.html'
+            })
+
+            .state('imprimer-creerunmodele', {
+                url: 'imprimer-creerunmodele',
+                templateUrl: 'partial-imprimer-creerunmodele.html'
+            })
+
+            .state('imprimer-exportervotremodele', {
+                url: 'imprimer-exportervotremodele',
+                templateUrl: 'partial-imprimer-exportervotremodele.html'
+            })
+
+            .state('imprimer-autres', {
+                url: 'imprimer-autres',
+                templateUrl: 'partial-imprimer-autres.html'
+            })
+
+            .state('panier', {
+                url: 'panier',
+                templateUrl: 'panier.html'
+            })
 
 
 
+            // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+            .state('about', {
+                // we'll get to this in a bit       
+            });
+});
 
+
+/**
+ * Controller panier d'achat
+ */
+app.controller("PanierCtrl", function($scope){
+    console.log("CHECKING OUT");
+    $scope.showCart = function(){
+        console.log("CHECKED OUT");
+    }
+});
