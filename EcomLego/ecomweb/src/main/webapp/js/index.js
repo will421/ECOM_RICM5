4c2870fa94772e03a573c57a2b29fd5eb36e9674
umpri5450 <umpri5450@gmail.com>
2014-12-13 22:00:10 +0100

    update

--- EcomLego/ecomweb/src/main/webapp/js/index.js
+++ EcomLego/ecomweb/src/main/webapp/js/index.js
@@ -1,4 +1,4 @@
-var app = angular.module('appMain', ['ui.bootstrap', 'ngResource', 'ui.router', 'ngCart'], function ($httpProvider) {
+var app = angular.module('appMain', ['ui.bootstrap', 'ngResource', 'ui.router', 'ngCart','angularFileUpload'], function ($httpProvider) {
     // Use x-www-form-urlencoded Content-Type
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 
@@ -118,9 +118,9 @@ app.config(function ($stateProvider, $urlRouterProvider) {
                 templateUrl: 'partial-imprimer-creerunmodele.html'
             })
 
-            .state('imprimer-exportervotremodele', {
-                url: 'imprimer-exportervotremodele',
-                templateUrl: 'partial-imprimer-exportervotremodele.html'
+            .state('imprimer-chargervotremodele', {
+                url: 'imprimer-chargervotremodele',
+                templateUrl: 'partial-imprimer-chargervotremodele.html'
             })
 
             .state('imprimer-autres', {
@@ -156,3 +156,59 @@ app.controller("PanierCtrl", function ($scope) {
         console.log("CHECKED OUT");
     }
 });
+
+/**
+ * File upload controller
+ */
+app.controller('FileUpload', ['$scope', 'FileUploader', function($scope, FileUploader) {
+        var uploader = $scope.uploader = new FileUploader({
+            url: 'upload.php'
+        });
+
+        // FILTERS
+
+        uploader.filters.push({
+            name: 'customFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+                return this.queue.length < 10;
+            }
+        });
+
+        // CALLBACKS
+
+        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+            console.info('onWhenAddingFileFailed', item, filter, options);
+        };
+        uploader.onAfterAddingFile = function(fileItem) {
+            console.info('onAfterAddingFile', fileItem);
+        };
+        uploader.onAfterAddingAll = function(addedFileItems) {
+            console.info('onAfterAddingAll', addedFileItems);
+        };
+        uploader.onBeforeUploadItem = function(item) {
+            console.info('onBeforeUploadItem', item);
+        };
+        uploader.onProgressItem = function(fileItem, progress) {
+            console.info('onProgressItem', fileItem, progress);
+        };
+        uploader.onProgressAll = function(progress) {
+            console.info('onProgressAll', progress);
+        };
+        uploader.onSuccessItem = function(fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+        };
+        uploader.onErrorItem = function(fileItem, response, status, headers) {
+            console.info('onErrorItem', fileItem, response, status, headers);
+        };
+        uploader.onCancelItem = function(fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteItem = function(fileItem, response, status, headers) {
+            console.info('onCompleteItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteAll = function() {
+            console.info('onCompleteAll');
+        };
+
+        console.info('uploader', uploader);
+    }]);
