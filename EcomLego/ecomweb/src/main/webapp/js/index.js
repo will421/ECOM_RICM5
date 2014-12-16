45f2aaeccd82c33698a1d73372ccd8b32e19a6ab
umpri5450 <umpri5450@gmail.com>
2014-12-16 11:13:52 +0100

    -Premier arrivée de testLionel sur la branche master -pas de merge necessaire, juste le dossier webapps ajouté

--- EcomLego/ecomweb/src/main/webapp/js/index.js
+++ EcomLego/ecomweb/src/main/webapp/js/index.js
@@ -1,4 +1,4 @@
-var app = angular.module('appMain', ['ui.bootstrap', 'ngResource', 'ui.router', 'ngCart','angularFileUpload'], function ($httpProvider) {
+var app = angular.module('appMain', ['ui.bootstrap', 'ngResource', 'ui.router', 'ngCart', 'angularFileUpload', 'restservice'], function ($httpProvider) {
     // Use x-www-form-urlencoded Content-Type
 
 });
@@ -6,24 +6,21 @@ var app = angular.module('appMain', ['ui.bootstrap', 'ngResource', 'ui.router', 
  * Controller menu navigation principal (Dropdown)
  */
 
+app.controller('DropdownCtrl', function ($scope, $log, $http) {
+    $scope.items = [
+        'The first choice!',
+        'And another choice for you.',
+        'but wait! A third!'
+    ];
+    $scope.status = {
+        isopen: false
+    };
 
+    $scope.toggled = function (open) {
+        $log.log('Dropdown is now: ', open);
+    };
+});
 
-/*
- app.controller('DropdownCtrl', function ($scope, $log, $http) {
- $scope.items = [
- 'The first choice!',
- 'And another choice for you.',
- 'but wait! A third!'
- ];
- $scope.status = {
- isopen: false
- };
- 
- $scope.toggled = function (open) {
- $log.log('Dropdown is now: ', open);
- };
- });
- */
 
 
 /**
@@ -108,18 +105,19 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 
 /**
  * Controller panier d'achat
+ * $scope is a MUST for REST service to work
  */
-app.controller("PanierCtrl", function ($scope) {
-    console.log("CHECKING OUT");
-    $scope.showCart = function () {
-        console.log("CHECKED OUT");
-    }
-});
+app.controller("PanierCtrl", ['$scope', 'Rest', function ($scope, Rest) {
+        console.log("CHECKING OUT");
+        $scope.showCart = function () {
+            console.log("CHECKED OUT");
+        }
+    }]);
 
 /**
  * File upload controller
  */
-app.controller('FileUpload', ['$scope', 'FileUploader', function($scope, FileUploader) {
+app.controller('FileUpload', ['$scope', 'FileUploader', function ($scope, FileUploader) {
         var uploader = $scope.uploader = new FileUploader({
             url: 'upload.php'
         });
@@ -128,46 +126,126 @@ app.controller('FileUpload', ['$scope', 'FileUploader', function($scope, FileUpl
 
         uploader.filters.push({
             name: 'customFilter',
-            fn: function(item /*{File|FileLikeObject}*/, options) {
+            fn: function (item /*{File|FileLikeObject}*/, options) {
                 return this.queue.length < 10;
             }
         });
 
         // CALLBACKS
 
-        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+        uploader.onWhenAddingFileFailed = function (item /*{File|FileLikeObject}*/, filter, options) {
             console.info('onWhenAddingFileFailed', item, filter, options);
         };
-        uploader.onAfterAddingFile = function(fileItem) {
+        uploader.onAfterAddingFile = function (fileItem) {
             console.info('onAfterAddingFile', fileItem);
         };
-        uploader.onAfterAddingAll = function(addedFileItems) {
+        uploader.onAfterAddingAll = function (addedFileItems) {
             console.info('onAfterAddingAll', addedFileItems);
         };
-        uploader.onBeforeUploadItem = function(item) {
+        uploader.onBeforeUploadItem = function (item) {
             console.info('onBeforeUploadItem', item);
         };
-        uploader.onProgressItem = function(fileItem, progress) {
+        uploader.onProgressItem = function (fileItem, progress) {
             console.info('onProgressItem', fileItem, progress);
         };
-        uploader.onProgressAll = function(progress) {
+        uploader.onProgressAll = function (progress) {
             console.info('onProgressAll', progress);
         };
-        uploader.onSuccessItem = function(fileItem, response, status, headers) {
+        uploader.onSuccessItem = function (fileItem, response, status, headers) {
             console.info('onSuccessItem', fileItem, response, status, headers);
         };
-        uploader.onErrorItem = function(fileItem, response, status, headers) {
+        uploader.onErrorItem = function (fileItem, response, status, headers) {
             console.info('onErrorItem', fileItem, response, status, headers);
         };
-        uploader.onCancelItem = function(fileItem, response, status, headers) {
+        uploader.onCancelItem = function (fileItem, response, status, headers) {
             console.info('onCancelItem', fileItem, response, status, headers);
         };
-        uploader.onCompleteItem = function(fileItem, response, status, headers) {
+        uploader.onCompleteItem = function (fileItem, response, status, headers) {
             console.info('onCompleteItem', fileItem, response, status, headers);
         };
-        uploader.onCompleteAll = function() {
+        uploader.onCompleteAll = function () {
             console.info('onCompleteAll');
         };
 
         console.info('uploader', uploader);
+    }]);
+
+
+/**
+ * Controller page des produits
+ */
+app.controller("ProduitsCtrl", ['$scope', 'Rest', function ($scope, Rest) {
+
+        $scope.resultProduits = [
+            {'Produit1': 'shitlego'},
+            {'Produit2': 'shitlego2'},
+            {'Produit3': 'shitlego3'},
+            {'Produit4': 'shitlego4'},
+            {'Produit5': 'shitlego5'},
+            {'Produit6': 'shitlego6'}
+        ]
+
+        $scope.getAllProduits = function () {
+            console.log("my ass");
+        }
+        $scope.getProduitsParPrixCroissant = function () {
+
+        }
+        $scope.getProduitsParPrixDecroissant = function () {
+
+        }
+        $scope.getProduitsParCouleur = function (col) {
+
+        }
+        $scope.getProduitsParTheme = function (theme) {
+
+        }
+        $scope.getProduitsParNom = function (nom) {
+
+        }
+    }]);
+
+
+/**
+ * Controller inscription
+ */
+app.controller("InscriptionCtrl", ['$scope', 'Rest', function ($scope, Rest) {
+        $scope.newSingleUser = {
+            'mail': '',
+            'mdp': '',
+            'mdpRetype': '',
+            'name': '',
+            'surname': '',
+            'numTel': '',
+            'numFix': '',
+            'adrLivraison': '',
+            'adrFacturation': '',
+            'newsletter': false
+        }
+
+        $scope.newSingleUserRestPost = new function () {
+            console.log("Sending POST to REST server with new user");
+            /**
+             * TEST POST 
+             */
+            Rest.POSTNEWSINGLEUSER.post();
+            console.log(posted);
+            console.log(posted.$promise.then(function (data) {
+                console.log(data);
+            }));
+        }
+
+        /*
+         * TEST GET
+         */
+//    var test = Rest.GETTEST.query();
+//    console.log(test);
+//    /*
+//     * Need to resolve the returned values 
+//     */
+//    console.log(test.$promise.then(function(data) {
+//       console.log(data.msg);
+//       console.log(data);
+//   }));
+
     }]);
